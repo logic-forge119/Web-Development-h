163c6994f4391b6e46b8cd64df7713e7d3f6e376
gajiul-dev5 <mdgajiul05@gmail.com>
2025-09-15 17:14:16 +0600

    new update

---
+++ video-57/loop.js
@@ -0,0 +1,39 @@
+// console.log("This is for loop in js");
+
+// let a=1;
+
+// for (let i = 0; i < 100; i++) {
+//   console.log(a+i);
+  
+// }
+
+// let obj={
+//   name:"Rakib",
+//   Role: "Programmer",
+//   Company:"Dev345"
+// }
+
+// for (const key in obj) {
+//   const element =obj[key];
+//   console.log(key,element);
+// }
+// for (const c of "Rakib") {
+//   console.log(c);
+// }
+
+//while loop;
+
+// let i=12;
+// while (i<=20) {
+//   console.log(i); 
+
+//   i++;
+// }
+
+//do-while loop
+
+let i=3;
+do {
+  console.log(i);
+  i++;
+} while (i<5);
