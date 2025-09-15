6ea93243f0d2284d34f11d8dc411a80014db1a3c
gajiul-dev5 <mdgajiul05@gmail.com>
2025-09-15 11:45:57 +0600

    new

---
+++ video-55/script.js
@@ -0,0 +1,29 @@
+// console.log("This is the second file of js")
+// var a= 5;
+// var b= 10;
+// console.log(a+b+20)
+
+//let= local variables;
+//var=global variables
+
+let x= "this is gazi here"
+let y= 119;
+let z=3.39;
+let p=true;
+let q= undefined;
+let r= null;
+console.log(x,y,z,p,q,r)
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+
+let o={
+  name:"Enter name here",
+  "jobe code":5600
+}
+console.log(o);
+//if we want to add new data= variables_name.what we add
+o.salary="100 crores"
+console.log(o);
+//if we update our data= main_variable_name.update_variable_name
+o.salary= "500 crores";
+console.log(o);
---
+++ video-56/script.js
@@ -0,0 +1,20 @@
+console.log("This is the video of condition in js")
+//In javascript semicolone is optional but using semicolone is good habit;
+
+let age=5;
+let grace= 2;
+console.log(age+grace);
+console.log(age-grace);
+console.log(age*grace);
+console.log(age/grace);
+console.log(age**grace);// that's means grace is power of  age variable;
+
+if((age+grace)>18){
+  console.log("You can drive");
+}
+else if(age==18){
+  console.log("You can aslo drive");
+}
+else{
+  console.log("You can't drive");
+}
