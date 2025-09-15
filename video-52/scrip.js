f2cbdd5d16501913729718673a2474f888a499c5
gajiul-dev5 <mdgajiul05@gmail.com>
2025-09-15 17:12:38 +0600

    new update

---
+++ video-52/scrip.js
@@ -0,0 +1,11 @@
+alert("Do you want to reload?");
+console.log("this is js")
+var a=prompt("Enter your number: ")
+console.log("Your number is " +a)
+var isTure =  confirm("Are you want to leave this page and blast your computer")
+if(isTure){
+  console.log("Computer is blasting")
+}
+else{
+  console.log("Your computer is NOT blasting")
+}
